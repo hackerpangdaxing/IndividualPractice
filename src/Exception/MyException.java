f59d7e5131af986a27d781b590d5017ed832e71c
hackerpangdaxing <hackerpangdaxing@gmail.com>
2017-09-20 17:31:29 +0800

    自定义异常

---
+++ src/Exception/MyException.java
@@ -0,0 +1,111 @@
+package Exception;
+
+/**
+ * Created by 胖大星 on 2017/9/20.
+ * 自定义异常类
+ * 如果希望写一个检查性异常类，则需要继承 Exception 类。如果你想写一个运行时异常类，那么需要继承 RuntimeException 类。
+ */
+public class MyException extends Exception {
+    static final long serialVersionUID = 1L;
+    //错误编码
+    private String errorCode;
+    /**
+     * 消息是否为属性文件中的Key
+     */
+    private boolean propertiesKey = true;
+    public  MyException(String message){
+        super(message);
+    }
+    /**
+     * 构造一个基本异常.
+     *
+     * @param errorCode
+     *            错误编码
+     * @param message
+     *            信息描述
+     */
+    public MyException(String errorCode, String message)
+    {
+        this(errorCode, message, true);
+    }
+
+    /**
+     * 构造一个基本异常.
+     *
+     * @param errorCode
+     *            错误编码
+     * @param message
+     *            信息描述
+     */
+    public MyException(String errorCode, String message, Throwable cause)
+    {
+        this(errorCode, message, cause, true);
+    }
+
+    /**
+     * 构造一个基本异常.
+     *
+     * @param errorCode
+     *            错误编码
+     * @param message
+     *            信息描述
+     * @param propertiesKey
+     *            消息是否为属性文件中的Key
+     */
+    public MyException(String errorCode, String message, boolean propertiesKey)
+    {
+        super(message);
+        this.setErrorCode(errorCode);
+        this.setPropertiesKey(propertiesKey);
+    }
+
+    /**
+     * 构造一个基本异常.
+     *
+     * @param errorCode
+     *            错误编码
+     * @param message
+     *            信息描述
+     */
+    public MyException(String errorCode, String message, Throwable cause, boolean propertiesKey)
+    {
+        super(message, cause);
+        this.setErrorCode(errorCode);
+        this.setPropertiesKey(propertiesKey);
+    }
+
+    /**
+     * 构造一个基本异常.
+     *
+     * @param message
+     *            信息描述
+     * @param cause
+     *            根异常类（可以存入任何异常）
+     */
+    public MyException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public String getErrorCode()
+    {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode)
+    {
+        this.errorCode = errorCode;
+    }
+
+    public boolean isPropertiesKey()
+    {
+        return propertiesKey;
+    }
+
+    public void setPropertiesKey(boolean propertiesKey)
+    {
+        this.propertiesKey = propertiesKey;
+    }
+
+
+}
