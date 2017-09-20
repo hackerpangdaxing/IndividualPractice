ec46d3d85894db90851f88517006cf67e73bdfba
hackerpangdaxing <hackerpangdaxing@gmail.com>
2017-09-20 17:34:21 +0800

    基础类型地址比较 equals与==

--- src/suanfa/Compare.java
+++ src/suanfa/Compare.java
@@ -1,10 +1,30 @@
 package suanfa;
 
+import java.util.Vector;
+
 /**
  * Created by 胖大星 on 2017/9/13.
  */
 public class Compare {
     public static void main(String[] args) {
+//        baseType();
+        Compare compare = new Compare();
+//        compare.baseType();
+        compare.compareTo();
+
+    }
+
+    public void compareTo(){
+        Vector vector1 = new Vector();
+        Vector vector2 = new Vector();
+        vector2 = vector1;
+        vector1 = null;
+        System.out.println("vector1 :" +vector1+ "     vector2: "+vector2);   //[]   null
+        System.out.println(vector1 == vector2);   //false
+    }
+
+
+    public  void baseType(){
         Integer s=new Integer(9);
         Integer t=new Integer(9);
         Long u=new Long(9);
@@ -16,7 +36,5 @@ public class Compare {
         System.out.println(s == t);  //false
         System.out.println(s == a);  //true
         System.out.println(u == a);  //true
-
-
     }
 }
